c812a4c3f1cae1a1650d117bdca7fb904bf97c35
Mateusz Wiktorowicz <wktorowicz.mat@gmail.com>
2024-01-16 09:30:08 +0100

    add mainMenu content page and welcome user func

---
+++ accountManager.js
@@ -0,0 +1,14 @@
+function welcomeLoggedInUser() {
+
+    var id = localStorage.getItem("idLoggedInUser")
+
+    if (id) {
+        userId = JSON.parse(id);
+        $("#welcomeNameBox").text(users[(userId - 1)].name + "!");
+    } else {
+        $("#welcomeNameBox").text("Friend!");
+    }
+} 
+
+
+welcomeLoggedInUser();
